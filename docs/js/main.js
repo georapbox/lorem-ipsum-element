3a9100e2b389a1277aa3356e8d3550c96f92fb11
George Raptis <georapbox@gmail.com>
2024-07-05 15:16:41 +0300

    Create interactive demo

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,6 +6,28 @@ const componentUrl = isLocalhost ? '../../dist/lorem-ipsum.js' : '../lib/lorem-i
 import(componentUrl).then(res => {
   const { LoremIpsum } = res;
   LoremIpsum.defineCustomElement();
+
+  const el = document.querySelector('lorem-ipsum');
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    const formData = new FormData(form);
+    const count = formData.get('count');
+    const lists = !!formData.get('lists');
+    const startWithLorem = !!formData.get('start-with-lorem');
+
+    el.setAttribute('count', count);
+    el.toggleAttribute('lists', lists);
+    el.toggleAttribute('start-with-lorem', startWithLorem);
+    el.generate();
+
+    const code = document.querySelector('#source-code');
+    const template = `<lorem-ipsum count="${count}"${lists ? ' lists' : ''}${startWithLorem ? ' start-with-lorem' : ''}></lorem-ipsum>`;
+    code.textContent = template;
+    window.hljs.highlightElement(code);
+  });
 }).catch(err => {
   console.error(err);
 });
